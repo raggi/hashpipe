abd703321a196282ee30f12e8dc192f986dd0d64
Erik Hollensbe <erik@hollensbe.org>
2010-06-05 05:50:15 -0400

    * More testing, working about the way I want it to now.

--- lib/hashpipe.rb
+++ lib/hashpipe.rb
@@ -5,37 +5,38 @@ module HashPipe
   # XXX we don't inherit from OpenStruct here because I'd have to play with its guts.
   #++
   class SuperOpenStruct
+    include Enumerable
 
     def self.create_accessor(sym, obj)
-      attr_accessor(sym)
+      sym = sym.to_sym
+      attr_accessor(sym) unless obj.respond_to?(sym)
     end
 
-    # similar to ivar_coerce only doesn't use instance variables
-    def [](sym_or_str)
-      sym = sym_or_str.to_sym
+    def initialize
+      class << self
+        def [](sym_or_str)
+          self.class.create_accessor(sym_or_str, self)
+          self.send(sym_or_str)
+        end
 
-      self.class.create_accessor(sym, self)
-      self.send(sym)
-    end
+        def []=(sym_or_str, value)
+          self.class.create_accessor(sym_or_str, self)
+          self.send("#{sym_or_str}=", value)
+          self.send(sym_or_str)
+        end
 
-    def []=(sym_or_str, value)
-      sym = sym_or_str.to_sym
-      self.class.create_accessor(sym, self)
-      self.send("#{sym}=", value)
-      self.send(sym)
-    end
+        def method_missing(sym, *args)
+          newsym = sym.to_s.gsub(/=$/, '').to_sym
+          self.class.create_accessor(newsym, self)
 
-    def method_missing(sym, *args)
-      newsym = sym.to_s.gsub(/=$/, '').to_sym
-      p newsym
-      self.class.create_accessor(newsym, self)
+          self.send(sym, *args)
+        end
 
-      self.send(sym, *args)
-    end
-
-    def lock!
-      def method_missing(*args)
-        raise ArgumentError, "this openstruct is locked."
+        def lock!
+          def method_missing(*args)
+            raise ArgumentError, "this openstruct is locked."
+          end
+        end
       end
     end
   end
@@ -46,7 +47,12 @@ if $0 == __FILE__
   h.foo = "bar"
   h.bar = "baz"
   h.lock!
+  p h['bar']
   p h[:bar]
   p h.foo
-  p h.quux
+  p h.quux rescue nil
+
+  h2 = HashPipe::SuperOpenStruct.new
+  h2.quux = "quux"
+  p h2.quux
 end
