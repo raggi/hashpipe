f4360928605b6adc3e15205791c014e3426b46d4
Erik Hollensbe <erik@hollensbe.org>
2010-06-07 00:41:35 -0400

    * New tests, changes as suggested by James Tucker. Thanks!

--- lib/hashpipe.rb
+++ lib/hashpipe.rb
@@ -12,50 +12,46 @@ class HashPipe
 
   include Enumerable
 
+  def keys
+    instance_variables.map { |ivar| self.class.callable_ivar(ivar) } 
+  end
+
+  def values
+    instance_variables.map { |ivar| self.send(self.class.callable_ivar(ivar)) }
+  end
+
+  def each
+    instance_variables.each do |ivar|
+      callable = self.class.callable_ivar(ivar)
+      yield(callable, self.send(callable))
+    end
+  end
+
   def initialize
-    class << self
-      def [](sym_or_str)
-        self.class.create_accessor(sym_or_str, self) unless respond_to?(sym_or_str)
-        self.send(sym_or_str)
-      end
+    def [](sym_or_str)
+      self.class.create_accessor(sym_or_str, self) unless respond_to?(sym_or_str)
+      self.send(sym_or_str)
+    end
 
-      def []=(sym_or_str, value)
-        self.class.create_accessor(sym_or_str, self) unless respond_to?(sym_or_str)
-        self.send("#{sym_or_str}=", value)
-        self.send(sym_or_str)
-      end
+    def []=(sym_or_str, value)
+      self.class.create_accessor(sym_or_str, self) unless respond_to?(sym_or_str)
+      self.send("#{sym_or_str}=", value)
+      self.send(sym_or_str)
+    end
 
-      def method_missing(sym, *args)
-        newsym = sym.to_s.gsub(/=$/, '').to_sym
-        if respond_to?(newsym) 
-          raise ArgumentError, "this method is reserved"
-        else
-          self.class.create_accessor(newsym, self) 
-          self.send(sym, *args)
-        end
+    def method_missing(sym, *args)
+      newsym = sym.to_s.gsub(/=$/, '').to_sym
+      if respond_to?(newsym) 
+        raise ArgumentError, "this method is reserved"
+      else
+        self.class.create_accessor(newsym, self) 
+        self.send(sym, *args)
       end
+    end
 
-      def keys
-        instance_variables.map { |ivar| self.class.callable_ivar(ivar) } 
-      end
-
-      def values
-        instance_variables.map { |ivar| self.send(self.class.callable_ivar(ivar)) }
-      end
-
-      def each
-        instance_variables.each do |ivar|
-          callable = self.class.callable_ivar(ivar)
-          yield(callable, self.send(callable))
-        end
-      end
-
-      def lock!
-        class << self
-          def method_missing(*args)
-            raise ArgumentError, "this openstruct is locked."
-          end
-        end
+    def lock!
+      class << self
+        self.send(:undef_method, :method_missing)
       end
     end
   end
