a6beaaa18aca0fd359af0b4788e8440921d6cfe1
raggi <jftucker@gmail.com>
2010-06-07 02:10:31 -0300

    harden against [:respond_to?] etc

--- lib/hashpipe.rb
+++ lib/hashpipe.rb
@@ -1,9 +1,11 @@
 class HashPipe
   def self.create_accessor(sym, obj)
-    class << obj; self; end.attr_accessor(sym)
+    unless obj.__respond_to?(sym)
+      class << obj; self; end.attr_accessor(sym)
+    end
   end
 
-  if :foo.respond_to?(:[]) # ruby1.9
+  if :foo.respond_to?(:[]) # ruby1.9 optimization
     def self.callable_ivar(ivar)
       ivar[/^@?(.*)$/, 1].to_sym
     end
@@ -17,39 +19,46 @@ class HashPipe
     public :attr_accessor
   end
 
+  alias __class__ class unless defined?(__class__) # rbx has it.
+  alias __instance_variables__ instance_variables
+  alias __respond_to? respond_to?
+  instance_methods.each { |m| undef_method m unless m =~ /^__|\?$/ }
+
   include Enumerable
 
   def [](sym_or_str)
-    self.class.create_accessor(sym_or_str, self) unless respond_to?(sym_or_str)
+    self.__class__.create_accessor(sym_or_str, self)
     self.__send__(sym_or_str)
   end
 
   def []=(sym_or_str, value)
-    self.class.create_accessor(sym_or_str, self) unless respond_to?(sym_or_str)
+    self.__class__.create_accessor(sym_or_str, self)
     self.__send__(:"#{sym_or_str}=", value)
   end
 
   def method_missing(sym, *args)
     newsym = sym.to_s.gsub(/=$/, '').to_sym
-    if respond_to?(newsym) 
+    if __respond_to?(newsym)
       raise ArgumentError, "this method is reserved"
     else
-      self.class.create_accessor(newsym, self) 
+      self.__class__.create_accessor(newsym, self) 
       self.__send__(sym, *args)
     end
   end
 
   def keys
-    instance_variables.map { |ivar| self.class.callable_ivar(ivar) } 
+    __instance_variables__.map { |ivar| self.__class__.callable_ivar(ivar) } 
   end
 
   def values
-    instance_variables.map { |ivar| self.send(self.class.callable_ivar(ivar)) }
+    __instance_variables__.map { |ivar|
+      self.__send__(self.__class__.callable_ivar(ivar))
+    }
   end
 
   def each
-    instance_variables.each do |ivar|
-      callable = self.class.callable_ivar(ivar)
+    __instance_variables__.each do |ivar|
+      callable = self.__class__.callable_ivar(ivar)
       yield(callable, self.__send__(callable))
     end
   end
